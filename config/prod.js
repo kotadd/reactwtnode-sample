271ae1c31ed30f844a253bd350e1baf11148b250
Kota Nishinaka <private2030kota@gmail.com>
2019-07-13 15:48:50 +0900

    finish auth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COKKIE_KEY
+};
+
+mongodb+srv://Userprod:Userpassword@cluster0-spcm2.mongodb.net/test?retryWrites=true&w=majority
